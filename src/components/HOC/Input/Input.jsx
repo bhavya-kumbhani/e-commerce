892e171abc80338a3703ca25f62ac6a26c19ae4c
bhavya kumbhani <bhavyakumbhani15@gmail.com>
2024-02-21 22:00:15 +0530

    added login page

--- src/components/HOC/Input/Input.jsx
+++ src/components/HOC/Input/Input.jsx
@@ -14,7 +14,7 @@ export default function Input({
 }){
     return(
         <div className="my-5">
-            <label htmlFor={labelFor} className="sr-only">
+            <label  className="sr-only">
               {labelText}
             </label>
             <input
