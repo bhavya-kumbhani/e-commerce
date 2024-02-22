6e5d529ae990383911cf28e5a21f4c94ce6991e6
fenilsavaliya30 <fenilsavaliya30@gmail.com>
2024-02-22 23:25:48 +0530

    some issue fix

--- src/routes/Landingpage/OurSevices/index.jsx
+++ src/routes/Landingpage/OurSevices/index.jsx
@@ -4,9 +4,9 @@ export default function OurSevices() {
     return (
         <section className="service container mx-auto py-8 px-6 h-auto">
             <div>
-                <h2 className="text-black font-bold text-4xl uppercase relative h-auto w-full pb-1 text-center ">
-                    Our Services
-                </h2>
+                <div className="flex flex-col justify-center items-center ">
+                    <h1 className="text-3xl xl:text-4xl font-semibold leading-7 xl:leading-9 text-gray-800 dark:text-white pb-[40px]">Feature Product</h1>
+                </div>
                 <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5">
                     <div className=" bg-gray-50 p-10 transition-all my-5 rounded flex flex-col justify-start items-start hover:shadow-xl">
 
